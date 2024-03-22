f0ac8aec896f19ca1b44a42f6eb39aed84cde2b1
Fernando <fernando.trejos10g@gmail.com>
2024-03-22 15:44:41 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Money-Manager',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
