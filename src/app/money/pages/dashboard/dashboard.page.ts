1ddd8c6a562f867915f8aa7cfcb30d28c6410c58
Fernando <fernando.trejos10g@gmail.com>
2024-03-24 16:22:03 -0500

    feat: angular 16

--- src/app/money/pages/dashboard/dashboard.page.ts
+++ src/app/money/pages/dashboard/dashboard.page.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'ft-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, HeaderComponent ]
 })
 export class DashboardPage implements OnInit {
 
